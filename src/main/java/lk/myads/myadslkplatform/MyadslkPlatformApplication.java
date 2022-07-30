21650f8ee6712e559aefd05033bd949c6294db7d
Chamud Sachintha <sheraanmario777@gmail.com>
2022-07-30 21:52:05 +0530

    initial commit

---
+++ src/main/java/lk/myads/myadslkplatform/MyadslkPlatformApplication.java
@@ -0,0 +1,13 @@
+package lk.myads.myadslkplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyadslkPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyadslkPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/myads/myadslkplatform/MyadslkPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.myads.myadslkplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyadslkPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
